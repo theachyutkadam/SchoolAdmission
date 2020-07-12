4685a28b850bc0a003dc688f3d5c6dadba98715a
ak <achyut.kadam@definelabs.com>
2020-07-12 21:09:14 +0530

    added user authentication spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   belongs_to :role
+  validates :username, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  def self.authenticate provided_username, provided_password
+    user = User.find_by(username: provided_username, password: provided_password)
+    if user
+      return true
+    else
+      return false
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#create' do
+    it 'should give error message if username is empty' do
+      user = User.new
+      user.save
+      expect(user.errors.messages[:username].first).to eq("can't be blank")
+    end
+
+    it 'should give error message if password is empty' do
+      user = User.new({username: "username"})
+      user.save
+      expect(user.errors.messages[:password].first).to eq("can't be blank")
+    end
+
+    it 'should give error message if password length less than 6 charactors' do
+      user = User.new({username: "username", password: "654"})
+      user.save
+      expect(user.errors.messages[:password].first).to eq("is too short (minimum is 6 characters)")
+    end
+  end
+
+  context "authenticate" do
+    it "should not authenticate if credentials are invalid" do
+      is_valid = User.authenticate("invalid_username", "invalid_password")
+      expect(is_valid).to be_falsey
+    end
+
+    it "should authenticate if credentials are valid" do
+      role = Role.create({name: 'student'})
+      user = User.new({username: "username", password: "123456", role_id: role.id})
+      user.save
+      is_valid = User.authenticate("username", "123456")
+      expect(is_valid).to be_truthy
+    end
+  end
 end
