d26051f31dc61ff3ee3c8c694f48464e473a9458
ak <achyut.kadam@definelabs.com>
2020-07-12 19:47:00 +0530

    Add validation for role model

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#create' do
+    it 'should create new role' do
+      Role.create({name: 'student'})
+      expect(Role.count).to eq(1)
+    end
+
+    it 'it should not create role if name is empty' do
+      role = Role.new({name: ''})
+      role.save
+      expect(Role.count).to eq(0)
+    end
+
+    it 'it should give error message if name is empty' do
+      role = Role.new({name: ''})
+      role.save
+      expect(role.errors.messages[:name].first).to eq("can't be blank")
+    end
+  end
 end
