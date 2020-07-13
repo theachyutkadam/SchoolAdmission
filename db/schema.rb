cf16184458252e8282f305332b92cab7d63bfe5e
ak <achyut.kadam@definelabs.com>
2020-07-13 17:04:15 +0530

    added schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_102353) do
+ActiveRecord::Schema.define(version: 2020_07_13_113132) do
+
+  create_table "classrooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "division"
+    t.string "medium"
+    t.bigint "teacher_id", null: false
+    t.bigint "standard_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["standard_id"], name: "index_classrooms_on_standard_id"
+    t.index ["teacher_id"], name: "index_classrooms_on_teacher_id"
+  end
 
   create_table "roles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -24,6 +35,25 @@ ActiveRecord::Schema.define(version: 2020_07_13_102353) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "teachers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "first_name"
+    t.string "middle_name"
+    t.string "last_name"
+    t.string "mother_name"
+    t.string "gender"
+    t.date "date_of_birth"
+    t.date "date_of_join"
+    t.string "aadhaar_card_number"
+    t.string "education"
+    t.string "speciality"
+    t.string "contact"
+    t.string "alternate_contact"
+    t.text "address"
+    t.boolean "is_active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "username"
     t.string "password"
@@ -33,5 +63,7 @@ ActiveRecord::Schema.define(version: 2020_07_13_102353) do
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "classrooms", "standards"
+  add_foreign_key "classrooms", "teachers"
   add_foreign_key "users", "roles"
 end
