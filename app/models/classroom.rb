12bdf1084ad338dcc71ec697dea476b71fac24ad
ak <achyut.kadam@definelabs.com>
2020-07-13 23:31:01 +0530

    added classroom model specs

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,4 +1,6 @@
 class Classroom < ApplicationRecord
   belongs_to :teacher
   belongs_to :standard
+  validates :medium, presence: true
+  validates :division, presence: true
 end
--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -19,11 +19,52 @@ RSpec.describe Classroom, type: :model do
         address: "",
         is_active: true 
       })
-      classroom = Classroom.new({medium: "marathi", standard_id: standard.id, teacher_id: teacher.id})
-      classroom.save
+      Classroom.new({division: 'A', medium: "marathi", standard_id: standard.id, teacher_id: teacher.id}).save
       expect(Classroom.count).to eq(1)
     end
-    # it 'it should not create new classroom' do
-    # end
+
+    it 'should not create new classroom if medium is empty' do
+      standard = Standard.create({name: "first"})
+      teacher = Teacher.create({
+        first_name: "rajesh",
+        middle_name: "vikas",
+        last_name: "kale",
+        mother_name: "anita",
+        date_of_birth: Date.current - 35.year,
+        date_of_join: Date.current - 10.year,
+        aadhaar_card_number: "111111111111",
+        education: "M.C.A",
+        speciality: "Programming",
+        contact: "9879879870",
+        alternate_contact: "9879879870",
+        address: "",
+        is_active: true 
+      })
+      Classroom.new({division: 'A', medium: '', standard_id: standard.id, teacher_id: teacher.id}).save
+      expect(Classroom.count).to eq(0)
+    end
+
+    it 'should give error message if division is empty' do
+      standard = Standard.create({name: "first"})
+      teacher = Teacher.create({
+        first_name: "rajesh",
+        middle_name: "vikas",
+        last_name: "kale",
+        mother_name: "anita",
+        date_of_birth: Date.current - 35.year,
+        date_of_join: Date.current - 10.year,
+        aadhaar_card_number: "111111111111",
+        education: "M.C.A",
+        speciality: "Programming",
+        contact: "9879879870",
+        alternate_contact: "9879879870",
+        address: "",
+        is_active: true 
+      })
+      classroom = Classroom.new({division: '', medium: 'marathi', standard_id: standard.id, teacher_id: teacher.id})
+      classroom.save
+      expect(classroom.errors.messages[:division].first).to eq("can't be blank")
+    end
+
   end
 end
