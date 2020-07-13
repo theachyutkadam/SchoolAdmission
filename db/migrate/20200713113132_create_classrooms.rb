91091da1f4cfd8d3d3a98ffbf25280cff063e402
ak <achyut.kadam@definelabs.com>
2020-07-13 17:03:50 +0530

    added classroom & teacher model

---
+++ db/migrate/20200713113132_create_classrooms.rb
@@ -0,0 +1,12 @@
+class CreateClassrooms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :classrooms do |t|
+      t.string :division
+      t.string :medium
+      t.references :teacher, null: false, foreign_key: true
+      t.references :standard, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
