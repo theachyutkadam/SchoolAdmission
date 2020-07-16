0809b378425b202c7a4b1da670a894c5a516983a
ak <achyut.kadam@definelabs.com>
2020-07-16 22:54:26 +0530

    update with factory bot

---
+++ spec/factories/standard.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :standard do
+    name { "first_standard" }
+  end
+end
--- spec/models/standard_spec.rb
+++ spec/models/standard_spec.rb
@@ -2,20 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Standard, type: :model do
   context "#create" do
-    it 'shold create standard' do
-      Standard.create({name: "first"})
-      # create(:student)
+    it 'should create new standard' do
+      create(:standard)
       expect(Standard.count).to eq(1)
     end
 
     it 'it should not create standard if name is empty' do
-      standard = Standard.new({name: ''})
+      standard = build(:standard, name: '')
       standard.save
       expect(Standard.count).to eq(0)
     end
 
     it 'it should give error message if name is empty' do
-      standard = Standard.new
+      standard = build(:standard, name: '')
       standard.save
       expect(standard.errors.messages[:name].first).to eq("can't be blank")
     end
