9813a1f6443ef8b13e193706faca1fc451ae6c47
ak <achyut.kadam@definelabs.com>
2020-07-19 12:56:07 +0530

    added new setup with git flow init

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_113132) do
+ActiveRecord::Schema.define(version: 2020_07_19_063419) do
 
   create_table "classrooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "division"
     t.string "medium"
+    t.integer "intake"
     t.bigint "teacher_id", null: false
     t.bigint "standard_id", null: false
     t.datetime "created_at", precision: 6, null: false
