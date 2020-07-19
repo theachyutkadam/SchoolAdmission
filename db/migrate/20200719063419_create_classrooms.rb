65e80c3c600358b0045f10ba9ba6226f374c3fdf
ak <achyut.kadam@definelabs.com>
2020-07-19 12:07:55 +0530

    added intake column in classroom

--- db/migrate/20200719063419_create_classrooms.rb
+++ db/migrate/20200719063419_create_classrooms.rb
@@ -3,6 +3,7 @@ class CreateClassrooms < ActiveRecord::Migration[6.0]
     create_table :classrooms do |t|
       t.string :division
       t.string :medium
+      t.integer :intake
       t.references :teacher, null: false, foreign_key: true
       t.references :standard, null: false, foreign_key: true
 
