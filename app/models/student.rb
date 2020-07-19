6da4c8862212f7cc34b1b0f796a8b0402c2076a9
ak <achyut.kadam@definelabs.com>
2020-07-19 17:05:02 +0530

    added student validation spec

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ApplicationRecord
   belongs_to :state
   belongs_to :district
   belongs_to :taluka
+
+  validates :first_name,
+            :middle_name,
+            :last_name,
+            :mother_name,
+            :date_of_birth,
+            :aadhaar_card_number,
+            presence: true
+  validates :aadhaar_card_number, numericality: true, uniqueness: true, :length => {:is => 12}
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -17,6 +17,103 @@ RSpec.describe Student, type: :model do
     end
   end
 
+  context "#validations" do
+    it "is not valid if first_name is empty" do
+      student = build(:student, first_name: '')
+      student.save
+      expect(student).to_not be_valid
+    end
+
+    it "is not valid if middle_name is empty" do
+      student = build(:student, middle_name: '')
+      student.save
+      expect(student).to_not be_valid
+    end
+
+    it "is not valid if last_name is empty" do
+      student = build(:student, last_name: '')
+      student.save
+      expect(student).to_not be_valid
+    end
+
+    it "is not valid if mother_name is empty" do
+      student = build(:student, mother_name: '')
+      student.save
+      expect(student).to_not be_valid
+    end
+
+    it "is not valid if date_of_birth is empty" do
+      student = build(:student, date_of_birth: '')
+      student.save
+      expect(student).to_not be_valid
+    end
+
+    it "should give error message if first_name is empty" do
+      student = build(:student, first_name: '')
+      student.save
+      expect(student.errors.messages[:first_name].first).to eq("can't be blank")
+    end
+
+    it "should give error message if middle_name is empty" do
+      student = build(:student, middle_name: '')
+      student.save
+      expect(student.errors.messages[:middle_name].first).to eq("can't be blank")
+    end
+
+    it "should give error message if last_name is empty" do
+      student = build(:student, last_name: '')
+      student.save
+      expect(student.errors.messages[:last_name].first).to eq("can't be blank")
+    end
+
+    it "should give error message if mother_name is empty" do
+      student = build(:student, mother_name: '')
+      student.save
+      expect(student.errors.messages[:mother_name].first).to eq("can't be blank")
+    end
+
+    it "should give error message if date_of_birth is empty" do
+      student = build(:student, date_of_birth: '')
+      student.save
+      expect(student.errors.messages[:date_of_birth].first).to eq("can't be blank")
+    end
+
+    it "is not valid if aadhaar_card_number is empty" do
+      student = build(:student, aadhaar_card_number: '')
+      student.save
+      expect(student).to_not be_valid
+    end
+    context "#validation presence" do
+      it "should give error message if aadhaar_card_number is empty" do
+        student = build(:student, aadhaar_card_number: '')
+        student.save
+        expect(student.errors.messages[:aadhaar_card_number].first).to eq("can't be blank")
+      end
+    end
+    context "#validation numericality" do
+      it "should give error message if aadhaar_card_number is not number" do
+        student = build(:student, aadhaar_card_number: 'invalid')
+        student.save
+        expect(student.errors.messages[:aadhaar_card_number].first).to eq("is not a number")
+      end
+    end
+    context "#validation uniquness" do
+      it "should give error message if aadhaar_card_number already exits" do
+        create(:student)
+        student = build(:student, aadhaar_card_number: '121212121212')
+        student.save
+        expect(student.errors.messages[:aadhaar_card_number].first).to eq("has already been taken")
+      end
+    end
+    context "#validation length" do
+      it "should give error message if aadhaar_card_number length is not 12 numbers" do
+        student = build(:student, aadhaar_card_number: '123')
+        student.save
+        expect(student.errors.messages[:aadhaar_card_number].first).to eq("is the wrong length (should be 12 characters)")
+      end
+    end
+  end
+
   context "#ActiveRecord association" do
     it "should belongs_to state" do
       expect(Student.reflect_on_association(:state).macro).to eq(:belongs_to)
