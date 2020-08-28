2682a8cff1baf31a15b821ffc7833ed182ae2c11
ak <achyut.kadam@definelabs.com>
2020-08-29 01:41:59 +0530

    role view

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,3 +1,32 @@
 class WelcomesController < ApplicationController
-  def index; end
+  def index
+    user = User.find params[:user]
+    @user_login = user.role.name
+    if user.role.name == "Headmaster"
+      headmaster_dashboard
+    elsif user.role.name == "Teacher"
+      teacher_dashboard
+    else user.role.name == "Student"
+      student_dashboard
+    end
+  end
+
+  def student_dashboard
+    puts "****************"
+    puts "Student"
+    puts "****************"
+  end
+
+  def teacher_dashboard
+    puts "****************"
+    puts "Teacher"
+    puts "****************"
+  end
+
+  def headmaster_dashboard
+    puts "****************"
+    puts "Headmaster"
+    puts "****************"
+  end
+
 end
--- spec/requests/welcomes_request_spec.rb
+++ spec/requests/welcomes_request_spec.rb
@@ -9,7 +9,31 @@ RSpec.describe 'Welcomes', type: :request do
     it 'returns http success' do
       user = create(:user, username: 'ravi', password: '123456')
       post '/login', params: { login: { username: user.username, password: user.password } }
-      get '/welcomes/index'
+      get "/welcomes/index?user=#{user.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should student login' do
+      role = create(:role, name: 'Student')
+      user = create(:user, username: 'student', password: '123456', role_id: role.id)
+      post '/login', params: { login: { username: user.username, password: user.password } }
+      get "/welcomes/index?user=#{user.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should teacher login' do
+      role = create(:role, name: 'Teacher')
+      user = create(:user, username: 'teacher', password: '123456', role_id: role.id)
+      post '/login', params: { login: { username: user.username, password: user.password } }
+      get "/welcomes/index?user=#{user.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should headmaster login' do
+      role = create(:role, name: 'Headmaster')
+      user = create(:user, username: 'headmaster', password: '123456', role_id: role.id)
+      post '/login', params: { login: { username: user.username, password: user.password } }
+      get "/welcomes/index?user=#{user.id}"
       expect(response).to have_http_status(:success)
     end
   end
