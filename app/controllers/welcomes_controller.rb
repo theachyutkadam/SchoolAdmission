48c19fee990bc19d5306f338fae58b449470a134
ak <achyut.kadam@definelabs.com>
2020-09-17 20:43:22 +0530

    setup navbar with login logout screen

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,12 +1,17 @@
 class WelcomesController < ApplicationController
   def index
     @current_user_role = current_user.login_type
+    # @current_user_name = current_user.username
     if @current_user_role == 'Headmaster'
       @teachers = Teacher.all
       @students = Student.all
     elsif @current_user_role == 'Teacher'
-      @students = Teacher.find(current_user.login_id).classroom.students
+      teacher = Teacher.find(current_user.login_id)
+      @students = teacher.classroom.students
+      @current_user_name = "#{teacher.first_name + ' ' + teacher.last_name}"
     else @current_user_role == 'Student'
+      student = Student.find(current_user.login_id)
+      @current_user_name = "#{student.first_name + ' ' + student.last_name}"
     end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,8 @@ require("channels")
 import "bootstrap";
 import "./custom";
 import "../stylesheets/application";
-import ".../assets/stylesheets/user"
-// import "../stylesheets/user";
+import "../stylesheets/user";
+import "../stylesheets/layout_navbar";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
