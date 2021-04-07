3ce9b000a922412fec45118d87e5fb3de19c8e63
developer <developer@ITWORKSs-MacBook-Pro.local>
2021-04-07 19:01:25 +0530

    sf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user
-
+  before_action :authenticate_user
   def authenticate_user
+    # user_id = request.headers["X-AUTH-TOKEN"]
+    current_user = User.find_by_id(cookies[:user_id])
 
-    user_id = request.headers["X-AUTH-TOKEN"]
-    # current_user = User.find_by_id(cookies[:user_id])
-
-    current_user = User.find_by_id(user_id)
+    # current_user = User.find_by_id(user_id)
 
     redirect_to root_path if current_user.nil?
   end
